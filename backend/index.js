622aae19f74394d7ffd098f83ddcb7a37b749d0d
Bhargav <bh950065@dal.ca>
2024-03-29 13:27:44 -0300

    backend apis updated

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ const app =express();
 const port = 6000;
 const connectDB = require('./utils/dbConnection');
 const expenseRoutes=require('./routes/expenseRoutes');
+const userRoutes = require('./routes/userRoutes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/expense',expenseRoutes);
+app.use('/user',userRoutes);
 connectDB();
 app.listen(port,()=>{
     console.log("server running on port",port);
