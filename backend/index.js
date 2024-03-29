1d95de2a581dee1e0dee7e0a6a8eca44efdbc7a0
Jaskaran Singh <js356337@dal.ca>
2024-03-29 14:44:39 -0300

    routes change

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', userRoutes);
 app.use('/expense', expenseRoutes);
-app.use('/', documentUploadRoutes)
+app.use('/documents', documentUploadRoutes)
 
 connectDB();
 
